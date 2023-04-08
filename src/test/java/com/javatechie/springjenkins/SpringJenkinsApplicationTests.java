306b80ad2bd4f7e51a93ecf7523d9cef9142fa29
Guillermo Navarro Mancillas <navarrog@globalhitss.com>
2023-04-08 15:08:47 -0700

    second commit

--- src/test/java/com/javatechie/springjenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/javatechie/springjenkins/SpringJenkinsApplicationTests.java
@@ -18,6 +18,7 @@ class SpringJenkinsApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.info("Test case executing...");
+		logger.info("Test case executin second log statement...");
 		assertEquals(true, true);
 	}
 
